6ae06e93f839dd787c3d7c8e004d31318c6c082b
Pig <pig.priv@gmail.com>
2020-03-19 14:10:57 -0400

    s/TheStrix/PIPIPIG233666/g
    yay

--- setup/gitsetup.sh
+++ setup/gitsetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get install git -y
-git config --global user.name "TheStrix"
-git config --global user.email "parthbhatia98@gmail.com"
+git config --global user.name "PIPIPIG233666"
+git config --global user.email "pig.priv@gmail.com"
 echo Setting ssh, enter password if you want to set one
 ssh-keygen -t rsa
 echo Your public ssh key is
